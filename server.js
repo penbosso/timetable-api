32eced35d4f369f4c08afaa420bfbdb97a905bff
penbosso <ckbosso@st.ug.edu.gh>
2019-07-17 03:01:11 +0000

    Heroku deployment

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 import app from './app';
 import http from 'http';
 import chalk from 'chalk';
+// import process from 'process';
 // import open from'open';
 /* eslint-disable no-console */
 
 const config = require('./common/config/env.config');
 const server = http.createServer(app);
 
-server.listen(config.port, function(err){
+app.set('port', (process.env.PORT || config.port));
+server.listen(app.get('port'), function(err){
     if(err){
       console.log(err);
     }
