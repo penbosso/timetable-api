82dcf774746ae8fd43d1e9fa66de8eeb58b6a7a4
penbosso <ckbosso@st.ug.edu.gh>
2019-09-05 23:21:28 +0000

    Using Buffer from

--- auth/verify.user.middleware.js
+++ auth/verify.user.middleware.js
@@ -38,7 +38,7 @@ exports.isPasswordAndUserMatch = (req, res, next) => {
                       email: user[0].email,
                       permissionLevel: user[0].permissionLevel,
                       provider: 'email',
-                      name: user[0].firstName + ' ' + user[0].lastName,
+                      name: user[0].otherName + ' ' + user[0].lastName,
                   };
                   return next();
               } else {
